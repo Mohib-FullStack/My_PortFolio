101211609e7c35626c0d6bc35e386a80bee75bfd
Mohib-Fullstack <mohibur_rahman09@outlook.com>
2025-03-16 03:04:29 +0100

    include Django

---
+++ Server/portfolio/settings.py
@@ -0,0 +1,191 @@
+import os
+from datetime import timedelta
+from pathlib import Path
+
+from corsheaders.defaults import default_headers
+from decouple import config
+
+# Base directory of the project
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Security settings
+SECRET_KEY = config(
+    "SECRET_KEY", default="uik3z=ovrad_r_km6bt%))&2a^m!kx)md&#oi7x%ml1bdro0b1"
+)
+DEBUG = config("DEBUG", default=True, cast=bool)
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1").split(",")
+INTERNAL_IPS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],  # Define custom templates directory
+        "APP_DIRS": True,  # Use app directories for templates
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+
+# Installed applications
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "Home",
+    "About",
+    "Projects",
+    "Experience",
+    "Blog",
+    "Resume",
+    "User",
+    "corsheaders",
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "django_celery_beat",
+    "debug_toolbar",
+    "cloudinary",
+    "cloudinary_storage",
+    "haystack",
+]
+
+# Middleware
+MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+
+HAYSTACK_CONNECTIONS = {
+    "default": {
+        "ENGINE": "haystack.backends.whoosh_backend.WhooshEngine",
+        "PATH": BASE_DIR / "whoosh_index",
+    },
+}
+
+# Celery settings
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+
+# Stripe settings
+STRIPE_SECRET_KEY = config("STRIPE_SECRET_KEY")
+STRIPE_PUBLIC_KEY = config("STRIPE_PUBLIC_KEY")
+
+
+# CORS configuration
+CORS_ALLOWED_ORIGINS = config(
+    "CORS_ALLOWED_ORIGINS", default="http://localhost:5173"
+).split(",")
+CORS_ALLOW_HEADERS = list(default_headers) + ["Authorization", "Content-Type"]
+
+# Client URL
+CLIENT_URL = config("CLIENT_URL", default="http://localhost:5173")
+
+# URLs and WSGI settings
+ROOT_URLCONF = "portfolio.urls"
+WSGI_APPLICATION = "portfolio.wsgi.application"
+
+# Database settings
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("POSTGRES_DATABASE"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        "HOST": config("POSTGRES_HOST", default="localhost"),
+        "PORT": config("POSTGRES_PORT", default="5432"),
+        "OPTIONS": {"options": "-c search_path=public"},
+    }
+}
+
+# Cloudinary configuration
+CLOUDINARY_NAME = config("CLOUDINARY_NAME")
+CLOUDINARY_API = config("CLOUDINARY_API")
+CLOUDINARY_SECRET_KEY = config("CLOUDINARY_SECRET_KEY")
+
+if not all([CLOUDINARY_NAME, CLOUDINARY_API, CLOUDINARY_SECRET_KEY]):
+    raise ValueError("Cloudinary configuration is incomplete.")
+
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": CLOUDINARY_NAME,
+    "API_KEY": CLOUDINARY_API,
+    "API_SECRET": CLOUDINARY_SECRET_KEY,
+}
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+
+# Email configuration
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config("SMTP_USERNAME")
+EMAIL_HOST_PASSWORD = config("SMTP_PASSWORD")
+
+# Static files
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+MEDIA_URL = "/media/"
+
+# Authentication
+# AUTH_USER_MODEL = "User.User"
+
+# Django REST Framework configuration
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+
+# JWT settings
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": True,
+}
+
+# Password validation
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+]
+
+# Localization
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
+USE_I18N = True
+USE_TZ = True
+
+# Primary key type
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
