99e1512901fdf849d05d27d56bbb6bc736d7e685
Mohib-Fullstack <mohibur_rahman09@outlook.com>
2025-03-25 01:40:35 +0100

    update urls

--- Server/Contact/urls.py
+++ Server/Contact/urls.py
@@ -14,7 +14,7 @@ from django.urls import path
 from .views import contact_detail, contact_list, get_csrf_token
 
 urlpatterns = [
-    path("contacts/", contact_list, name="contact-list"),
-    path("contacts/<int:pk>/", contact_detail, name="contact-detail"),
+    path("contact/", contact_list, name="contact-list"),
+    path("contact/<int:pk>/", contact_detail, name="contact-detail"),
     path("csrf-token/", get_csrf_token, name="get-csrf-token"),
 ]
