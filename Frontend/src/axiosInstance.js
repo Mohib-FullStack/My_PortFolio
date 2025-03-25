d6ddc84f5b0ec9a1d64d099966f10b3a8a568952
Mohib-Fullstack <mohibur_rahman09@outlook.com>
2025-03-25 02:56:12 +0100

    update urls

--- Frontend/src/axiosInstance.js
+++ Frontend/src/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://my-portfolio-pmve.onrender.com/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://my-portfolio-pmve.onrender.com';
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
