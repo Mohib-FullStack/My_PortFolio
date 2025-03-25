0b0183dbc9be9028d46a7a3500100462e1d58b40
Mohib-Fullstack <mohibur_rahman09@outlook.com>
2025-03-25 02:38:07 +0100

    update rusl

--- Server/portfolio/urls.py
+++ Server/portfolio/urls.py
@@ -21,8 +21,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("Contact.urls")),  # Include Contact app URLs
-    path("api/", include("Resume.urls")),  # Include Resume app URLs
+    path("api/contacts", include("Contact.urls")),  # Include Contact app URLs
+    path("api/contacts", include("Resume.urls")),  # Include Resume app URLs
 ]
 
 
