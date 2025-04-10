7ff52cf1104acb88b3e67ab6a05c0a87f6f7d427
Mohib-Fullstack <mohibur_rahman09@outlook.com>
2025-04-10 02:59:40 +0200

    update contact

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { I18nextProvider } from "react-i18next";
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import store from '../app/store';
 import App from './App';
 import i18n from "./i18n";
@@ -17,14 +17,54 @@ createRoot(document.getElementById('root')).render(
       <Provider store={store}>
         <CssBaseline />
         <I18nextProvider i18n={i18n}>
-          <BrowserRouter basename="/">
+          <HashRouter>
             <App />
-          </BrowserRouter>
+          </HashRouter>
         </I18nextProvider>
       </Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
+
+
+
+
+
+
+
+
+// import { CssBaseline } from '@mui/material';
+// import { ThemeProvider } from '@mui/material/styles';
+// import React from 'react';
+// import { createRoot } from 'react-dom/client';
+// import { I18nextProvider } from "react-i18next";
+// import { Provider } from 'react-redux';
+// import { HashRouter } from 'react-router-dom';
+// import store from '../app/store';
+// import App from './App';
+// import i18n from "./i18n";
+// import './index.css';
+// import theme from './theme';
+
+// createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <ThemeProvider theme={theme}>
+//       <Provider store={store}>
+//         <CssBaseline />
+//         <I18nextProvider i18n={i18n}>
+//           {/* <BrowserRouter basename="/">
+//             <App />
+//           </BrowserRouter> */}
+
+//           // Change from BrowserRouter to HashRouter
+// <HashRouter basename="/">
+//   <App />
+// </HashRouter>
+//         </I18nextProvider>
+//       </Provider>
+//     </ThemeProvider>
+//   </React.StrictMode>
+// );
 
 
 
